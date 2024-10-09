1397c5954b45672110ff431c0bd19df88c3255d8
Emmatosorus <emma@bouteyres.com>
2024-10-09 14:48:42 +0200

    Remove 💥 && Move 🚚: moved parsing to sec files + removed main in parsing file

--- src/client_msg_parse.cpp
+++ src/client_msg_parse.cpp
@@ -2,34 +2,34 @@
 // Created by epolitze on 09/10/24.
 //
 
-#include "client_msg_parse.hpp"
+#include "../include/client_msg_parse.hpp"
 
-int main(int ac, char **av)
-{
-	std::vector<std::string>	vec;
-	if (ac < 2)
-		return 0;
-
-	vec = parse_client_msg(av[1]);
-
-	if (vec.empty())
-	{
-		std::cerr << "Invalid argument" << std::endl;
-		return (1);
-	}
-	size_t i = 0;
-	std::cout << BOLD << "Vector has :\n" << END;
-	for (std::vector<std::string>::iterator it = vec.begin(); it != vec.end(); it++)
-	{
-		if (i == 0)
-			std::cout << BOLD << "Command : " << END << *it << "\n";
-		else
-			std::cout << BOLD << "ARG" << i << " : " << END << *it << "\n";
-		i++;
-	}
-
-	std::cout << std::endl;
-}
+//int main(int ac, char **av)
+//{
+//	std::vector<std::string>	vec;
+//	if (ac < 2)
+//		return 0;
+//
+//	vec = parse_client_msg(av[1]);
+//
+//	if (vec.empty())
+//	{
+//		std::cerr << "Invalid argument" << std::endl;
+//		return (1);
+//	}
+//	size_t i = 0;
+//	std::cout << BOLD << "Vector has :\n" << END;
+//	for (std::vector<std::string>::iterator it = vec.begin(); it != vec.end(); it++)
+//	{
+//		if (i == 0)
+//			std::cout << BOLD << "Command : " << END << *it << "\n";
+//		else
+//			std::cout << BOLD << "ARG" << i << " : " << END << *it << "\n";
+//		i++;
+//	}
+//
+//	std::cout << std::endl;
+//}
 
 std::vector<std::string>	parse_client_msg(std::string input)
 {
