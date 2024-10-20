55e1ac671ce8c4bf51fa1bb941b4746e2205f343
pdlmn <jseldardw@gmail.com>
2024-10-20 19:26:29 +0200

    fix: fixed the message on when person quits

--- src/commands/quit.cpp
+++ src/commands/quit.cpp
@@ -6,9 +6,9 @@ void Server::_quit(PollfdIterator* it, const std::vector<std::string>& args)
 {
     Client& client = m_clients[(*it)->fd];
 
-    std::string quit_msg = ":" + client.nickname + " QUIT";
-    if (args.size() > 1)
-        quit_msg + ":" + args[1];
+	std::string quit_msg = ":" + client.nickname + " QUIT";
+	if (args.size() > 1)
+		quit_msg += " :" + args[1];
 
     for (size_t i = 0; i < client.channels.size(); i++)
     {
